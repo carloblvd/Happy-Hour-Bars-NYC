df9b8e061246d5007d476786acc561f673bd6884
carloblvd <carloblvd@gmail.com>
2023-09-25 16:54:10 -0400

    first commit

---
+++ src/BarData.jsx
@@ -0,0 +1,20 @@
+// Template { barName:"", daysAndHours:{Monday:"", Tuesday:"", Wednesday:"", Thursday:"", Friday:"", Saturday:"", Sunday:""},neighborhood:"", borough:"", food:  }
+
+export const BarData = [
+  {
+    barName: "A10 Kitchen",
+    daysAndHours: {
+      Monday: "none",
+      Tuesday: "4:00pm - 7:00pm",
+      Wednesday: "4:00pm - 7:00pm",
+      Thursday: "4:00pm - 7:00pm",
+      Friday: "4:00pm - 7:00pm",
+      Saturday: "1:00pm - 7:00pm",
+      Sunday: "1:00pm - 8:00pm",
+    },
+    borough: "Manhattan",
+    neighborhood: "East Village",
+    food: true,
+    link: "https://a10kitchen.com/",
+  },
+];
